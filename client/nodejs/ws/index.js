16de299565c00f34e591bb2c833a2206443574c6
Vic P <vic4key@gmail.com>
2022-08-25 11:18:27 +0700

    nodejs

--- client/nodejs/ws/index.js
+++ client/nodejs/ws/index.js
@@ -2,15 +2,15 @@ import WebSocket from "ws";
 
 const ws = new WebSocket("ws://127.0.0.1:1609/echo");
 
-ws.on("open", function open() {
+ws.on("open", () => {
   console.log("WS opened");
   ws.send("Howdy, I'm Client");
 });
 
-ws.on("close", function open() {
+ws.on("close", () => {
   console.log("WS closed")
 });
 
-ws.on("message", function message(data) {
+ws.on("message", (data) => {
   console.log("WS message", data.toString());
 });
